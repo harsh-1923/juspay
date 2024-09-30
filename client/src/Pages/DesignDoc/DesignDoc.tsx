cec0f580cc91fe2367a6659ba2333cf39b07f7bd
Harsh <harsh.ju.sharma@gmail.com>
2024-09-30 10:40:02 +0530

    Build fix

--- client/src/Pages/DesignDoc/DesignDoc.tsx
+++ client/src/Pages/DesignDoc/DesignDoc.tsx
@@ -1,17 +1,5 @@
-import Suchi from "suchi";
-
 const DesignDoc = () => {
-  return (
-    <div className="debug">
-      <Suchi.Root accentColor="blue">
-        <Suchi.Header>Design Docs</Suchi.Header>
-        <Suchi.Section>
-          <Suchi.SectionHeader>Header 1</Suchi.SectionHeader>
-          <Suchi.Content>askjdhgf;alksdjflk;jasdlkfjsald;fjalk</Suchi.Content>
-        </Suchi.Section>
-      </Suchi.Root>
-    </div>
-  );
+  return <div className="debug"></div>;
 };
 
 export default DesignDoc;
